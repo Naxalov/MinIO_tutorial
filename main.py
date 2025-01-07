d3092792602a52a517c01eb606d9f1d3f3aff56e
Zarifjon Nakhalov <naxalov@MacBook-Air-Zarifjon.local>
2025-01-07 16:14:39 +0500

    create client

--- main.py
+++ main.py
@@ -5,4 +5,13 @@ import os
 access_key = os.getenv('MINIO_ACCESS_KEY')
 secret_key = os.getenv('MINIO_SECRET_KEY')
 
-print(f'Access Key: {access_key} Secret Key {secret_key}')
+print(f'acc_key: "{access_key}" secret key "{secret_key}"')
+# Initialize the client
+client = minio.Minio(
+    "cdn.qarshidu.uz:9001", # Only the host name and port
+    access_key=access_key,
+    secret_key=secret_key,
+    secure=False
+)
+
+
