1a1b8ad5ee878115be9d5773320703968b24bd96
Zarifjon Nakhalov <naxalov@MacBook-Air-Zarifjon.local>
2025-01-07 11:25:52 +0500

    check credentials

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
 import minio
 
-# Print the version of MinIO server
-print(minio.__version__)
+import os
+# Get the access key and secret key from the environment
+access_key = os.getenv('MINIO_ACCESS_KEY')
+secret_key = os.getenv('MINIO_SECRET_KEY')
+
+print(f'Access Key: {access_key} Secret Key {secret_key}')
