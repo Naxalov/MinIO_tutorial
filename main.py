956d1c6b89ee715f37286c65e496cf3aa6eadc1b
Zarifjon Nakhalov <naxalov@MacBook-Air-Zarifjon.local>
2025-01-07 19:16:47 +0500

    Check if the bucket exists

--- main.py
+++ main.py
@@ -5,13 +5,18 @@ import os
 access_key = os.getenv('MINIO_ACCESS_KEY')
 secret_key = os.getenv('MINIO_SECRET_KEY')
 
-print(f'acc_key: "{access_key}" secret key "{secret_key}"')
-# Initialize the client
 client = minio.Minio(
-    "cdn.qarshidu.uz:9001", # Only the host name and port
-    access_key=access_key,
-    secret_key=secret_key,
-    secure=False
+    "play.minio.io:9000",
+    access_key="Q3AM3UQ867SPQQA43P2F",
+    secret_key="zuf+tfteSlswRu7BJ86wekitnifILbZam1KYY3TG",
+ 
 )
 
+BUCKET_NAME = "mybucket"
 
+# Check if the bucket already exists
+if client.bucket_exists(BUCKET_NAME):
+    print("Bucket already exists")
+else:
+    client.make_bucket(BUCKET_NAME)
+    print("Bucket created successfully")
